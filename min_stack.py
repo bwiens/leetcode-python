2039981ec212acf2ecb553ccd97c9bd9b358f378
Benjamin Wiens <benjamin.wiens@gmail.com>
2018-11-27 21:31:03 +0000

    Adding Leetcode Acceptance Status

--- min_stack.py
+++ min_stack.py
@@ -1,48 +1,47 @@
 #!/usr/bin/python
 #Benjamin Wiens
 #Min Stack (https://leetcode.com/problems/min-stack/description/)
+#Leetcode Accepted
 
 class MinStack:
-
     def __init__(self):
         """
         initialize your data structure here.
         """
+        self.stack = []
 
     def push(self, x):
         """
         :type x: int
         :rtype: void
         """
-        stack.append(x)
+        self.stack.append(x)
 
     def pop(self):
         """
         :rtype: void
         """
-        stack.pop(-1)
+        self.stack.pop(-1)
 
     def top(self):
         """
         :rtype: int
         """
-        return stack[-1]
-
+        if len(self.stack) > 0:
+            return self.stack[-1]
+        else:
+            return None
     def getMin(self):
         """
         :rtype: int
         """
-        min = stack[0]
-        for i in range(len(stack)):
-            if min > stack[i]:
-                min = stack[i]
-        return min
+        if len(self.stack) > 0:
+            mins = min(self.stack)
+            return mins
+        else:
+            return None
 
 minStack = MinStack()
-minStack.push(-2)
-minStack.push(0)
-minStack.push(-3)
-print(minStack.getMin())   #--> Returns -3.
-minStack.pop()
-print(minStack.top())      #--> Returns 0.
-print(minStack.getMin())   #--> Returns -2.
+minStack.push(-1)
+print(minStack.top())
+print(minStack.getMin())
