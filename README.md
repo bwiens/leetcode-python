8b2fd4d79aecedbb49d49850253f4d836fca8f42
Benjamin Wiens <benjamin.wiens@gmail.com>
2019-01-16 20:01:57 +0000

    Adding Sliding Window Maximum

--- README.md
+++ README.md
@@ -40,3 +40,4 @@ Hard:
 - Longest Substring with At Most K Distinct Characters (https://leetfree.com/problems/longest-substring-with-at-most-k-distinct-characters.html)
 - Candy (https://leetcode.com/problems/candy/)
 - Median of Two Sorted Arrays (https://leetcode.com/problems/median-of-two-sorted-arrays/)
+- Sliding Window Maximum (https://leetcode.com/problems/sliding-window-maximum/)
---
+++ sliding_window_maximum.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+#Benjamin Wiens
+#Sliding Window Maximum https://leetcode.com/problems/sliding-window-maximum/
+#Leetcode Accepted
+
+input = [1,3,-1,-3,5,3,6,7]
+k = 3
+
+class Solution:
+    def maxSlidingWindow(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        result = []
+        max, current, c = 0, 0, 1
+        if not nums:
+            return []
+        if k == 1:
+            return nums  
+        for index, i in enumerate(nums):
+            max = i
+            while c < k:
+                if index + k < len(nums) +1:
+                    if max < nums[index + c]:
+                        max = nums[index + c]
+                    c += 1
+                else:
+                    return result
+            result.append(max)
+            c = 1
+            max = 0
+
+print(Solution().maxSlidingWindow(input, k))
