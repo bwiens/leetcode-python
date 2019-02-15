7cae7e5353f8310acabea6a8b910ec9d09d4bcbe
Benjamin Wiens <benjamin.wiens@gmail.com>
2019-02-15 03:57:26 +0000

    Reworked Code, Leetcode Accepted

--- happy_number.py
+++ happy_number.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python
 #Benjamin Wiens
 #https://leetcode.com/problems/happy-number/
-import math
-input = 19
+#Leetcode Accepted
+input = 3
 class Solution:
-    def isHappy(self, n):
-        """
-        :type n: int
-        :rtype: bool
-        """
-        sum = 0
-        #create a hash table
-        numbers = dict(enumerate(str(input)))
-        while sum != 1:
-            sum = 0
-            #take the square
-            for key, value in numbers.items():
-                sum += int(value) * int(value)
-            #update the hash table
-            numbers = dict(enumerate(str(sum)))
-        return True
-
+    def happyAux(self, n, dict) -> 'bool':
+        y=0
+        for i in str(n):
+            y += int(i) **2
+        if y == 1:
+            return True
+        else:
+            if y in dict:
+                return False
+            else:
+                dict[y] = None
+                return self.happyAux(y, dict)
+    def isHappy(self, n: 'int') -> 'bool':
+        dict = {}
+        return self.happyAux(n, dict)
 print(Solution().isHappy(input))
