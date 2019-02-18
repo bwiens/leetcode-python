fb48997b6318f8a2e92e75533fd236c68a6c8aee
Benjamin Wiens <benjamin.wiens@gmail.com>
2019-02-18 01:32:45 +0000

    Optimal Solution for Jewels and Stones

--- jewels_and_stones.py
+++ jewels_and_stones.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 #Benjamin Wiens
-#Jewels and Stones case sensitive iteratively
+#Jewels and Stones (https://leetcode.com/problems/jewels-and-stones/)
+#Leetcode Accepted
 
 #Jewels
 J1 = "aA"
@@ -9,23 +10,15 @@ J2 = "z"
 S1 = "aAAbbbb"
 S2 = "ZZ"
 
-
-class Solution(object):
-    def numJewelsInStones(self, J, S):
-        """
-        :type J: str
-        :type S: str
-        :rtype: int
-        """
-        c=0
-        i = j = 0
-        #iterate through stones for each jewel
-        for i in range(len(J)):
-            for j in range(len(S)):
-                if J[i] == S[j]:
-                    c+=1
-        return c
+class Solution:
+    def numJewelsInStones(self, J: 'str', S: 'str') -> 'int':
+        if not J:
+            return 0
+        jewels = 0
+        for i in S:
+            if i in J:
+                jewels += 1
+        return jewels
 
 print(Solution().numJewelsInStones(J1, S1))
 print(Solution().numJewelsInStones(J2, S2))
-#Since we have two for loops, the time complexity is O(n^2)
