7be77f981ebc98ac0e2b4db28e460c123bfbf0bb
Benjamin Wiens <benjamin.wiens@gmail.com>
2019-09-08 16:57:49 +0000

    Comments for Two City Scheduling

--- two_city_scheduling.py
+++ two_city_scheduling.py
@@ -10,6 +10,8 @@ class Solution:
         costs.sort(key=lambda cost: cost[0] - cost[1])
         # then go greedy
         result = 0
+	# add the first half with cheapest to get to city A first, then B
+	# remember the requirement is half go to A, half to B
         for cost in costs[:len(costs)//2]:
             result += cost[0]
         for cost in costs[len(costs)//2:]:
