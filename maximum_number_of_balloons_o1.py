2c2f8e01aadb82ebb6c35b3c920073afbd7a60b1
root <root@raspberrypi.(none)>
2019-09-16 12:34:06 +0000

    Adding O(1) Space version of Maximum Number of Balloons

---
+++ maximum_number_of_balloons_o1.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+# Benjamin Wiens
+# Maximum Number of Balloons (https://leetcode.com/problems/maximum-number-of-balloons/)
+
+balloons = "loonbalxballpoon"
+
+import sys
+class Solution:
+    def maxNumberOfBalloons(self, text: str) -> int:
+        minimum = sys.maxsize 
+        hmap = {1: 'b', 2: 'a', 3: 'l', 4: 'o', 5: 'n'}
+        for key, value in hmap.items():
+            if value == 'l' or value == 'o':
+                minimum = min(minimum, text.count(value)//2)
+            else:
+                minimum = min(minimum, text.count(value))
+        return minimum
+ 
+print(Solution().maxNumberOfBalloons(balloons))
