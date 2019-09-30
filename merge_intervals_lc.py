9f3a13f771efc01fe13e4b1ac76414f7186a7f49
Benjamin Wiens <benjamin.wiens@gmail.com>
2019-09-30 19:15:15 +0000

    Adding Better Version of Merge Intervals

--- merge_intervals_lc.py
+++ merge_intervals_lc.py
@@ -1,38 +1,20 @@
 #!/usr/bin/python
-#Benjamin Wiens
-#Merge Intervals (https://leetcode.com/problems/merge-intervals/)
-#Leetcode Accepted
-#The odd leetcode variant with intervals as objects
+# Benjamin Wiens
+# Merge Intervals (https://leetcode.com/problems/merge-intervals/)
 
-
-#input = [[1,3],[2,6],[8,10],[15,18]]
-#input = [Interval(1,3),Interval(2,6),Interval(8,10),Interval(15,18)]
-# Definition for an interval.
-class Interval:
-    def __init__(self, s=0, e=0):
-        self.start = s
-        self.end = e
-#    def __repr__(self):
-#        return f"Interval({self.start}, {self.end})"
+input = [[1,4],[2,3]]
 
 class Solution:
     def merge(self, intervals):
-        #sort the intervals
-        #for i in intervals:
-        #    Interval(*i)
-        intervals = [Interval(1, 3), Interval(2, 6), Interval(8, 10), Interval(15, 18)]
-        intervals.sort(key=lambda x: x.start)
-        result, merged = [], []
-        tmp = []
-        for i in intervals:
-            if not merged:
-                merged.append(i)
-            elif i.start <= merged[-1].end:
-                    tmp = merged[-1]
-                    if merged[-1].end < i.end:
-                        merged[-1].end = i.end
+        if not intervals:
+            return []
+        intervals.sort()
+        result = [intervals[0]]
+        for interval in intervals[1:]:
+            if interval[0] <= result[-1][1]:
+                result[-1][1] = max(interval[1],result[-1][1])
             else:
-                merged.append(i)
-        return merged
+                result.append(interval)
+        return result
 
 print(Solution().merge(input))
