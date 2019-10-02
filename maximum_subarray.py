11596da21416128091c7f9db86d83fe2be6e6b0b
Benjamin Wiens <benjamin.wiens@gmail.com>
2019-10-02 14:38:28 +0000

    Better Version of Maximum Subarray

--- maximum_subarray.py
+++ maximum_subarray.py
@@ -5,20 +5,13 @@
 
 input = [-2,1,-3,4,-1,2,1,-5,4]
 
-class Solution(object):
+class Solution:
     def maxSubArray(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
+        rolling = nums[0]
         maximum = nums[0]
-        rolling_max = nums[0]
-        for e in nums[1:]:
-            #we can drop the rolling max when 
-            #the current element is bigger
-            rolling_max = max(e, rolling_max+e)
-            if maximum < rolling_max:
-                maximum = rolling_max
+        for n in nums[1:]:
+            rolling = max(n, rolling+n)
+            maximum = max(maximum, rolling)
         return maximum
 
 print(Solution().maxSubArray(input))
