11e7f55d86f0d49cc1e333c36a1c8773a6e4abfe
Benjamin Wiens <benjamin.wiens@gmail.com>
2019-10-02 13:54:27 +0000

    Improved Version (One Loop) of Reverse Vowels of a String

--- reverse_vowels_of_a_string.py
+++ reverse_vowels_of_a_string.py
@@ -2,7 +2,6 @@
 # Benjamin Wiens
 # Reverse Vowels of a String
 # https://leetcode.com/problems/reverse-vowels-of-a-string
-# Leetcode Accepted
 
 input = "leetcode"
 
@@ -14,16 +13,21 @@ class Solution:
         """
         vowels = ['e', 'a', 'u', 'o', 'i', 'A', 'E', 'U', 'O', 'I']
         s_list = list(s)
-        stack = []
-        for i in s_list:
-            if i in vowels:
-                stack.append(i)
-        #print(stack)
-        for index, i in enumerate(s_list):
-            if i in vowels:
-                s_list[index] = stack[-1]
-                stack.pop()
-        return(''.join(s_list))
-
+        l = 0
+        r = len(s_list) - 1
+        while l < r:
+            if s_list[l] in vowels and s_list[r] not in vowels:
+                r -= 1
+            elif s_list[r] in vowels and s_list[l] not in vowels:
+                l += 1
+            elif s_list[r] in vowels and s_list[l] in vowels:
+                s_list[r], s_list[l] = s_list[l], s_list[r]
+                r -= 1
+                l += 1
+            #none are in vowels
+            else:
+                r -= 1
+                l += 1
+        return ''.join(s_list)
 
 print(Solution().reverseVowels(input))
