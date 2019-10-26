56c31de4514d6dcba182d006cfdbe1f21a0eedc2
Benjamin Wiens <benjamin.wiens@gmail.com>
2019-10-26 19:45:10 +0000

    Better code for Sentence Similarity

--- sentence_similarity.py
+++ sentence_similarity.py
@@ -5,24 +5,18 @@
 words1 = ["great","acting","skills"]
 words2 = ["fine","drama","talent"]
 pairs = [["great","fine"],["drama","acting"],["skills","talent"]]
+
+from collections import defaultdict
 class Solution:
     def areSentencesSimilar(self, words1, words2, pairs):
         if not pairs:
             return words1 == words2
         if len(words1) != len(words2):
             return False
-        hmap = {}
+        hmap = defaultdict(list)
         for pair in pairs:
-            if pair[0] not in hmap:
-                hmap[pair[0]] = []
-                hmap[pair[0]].append(pair[1])
-            else:
-                hmap[pair[0]].append(pair[1])
-            if pair[1] not in hmap:
-                hmap[pair[1]] = []
-                hmap[pair[1]].append(pair[0])
-            else:
-                hmap[pair[1]].append(pair[0])
+            hmap[pair[0]].append(pair[1])
+            hmap[pair[1]].append(pair[0])
         ##########################################
         for index, word in enumerate(words1):
             if words1[index] == words2[index]:
@@ -30,5 +24,4 @@ class Solution:
             if word not in hmap or words2[index] not in hmap[word]:
                 return False
         return True
-
 print(Solution().areSentencesSimilar(words1, words2, pairs))
