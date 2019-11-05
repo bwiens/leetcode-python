2f2f4b0e922048c173a43f54dd69b5bf36f56278
Benjamin Wiens <benjamin.wiens@gmail.com>
2019-11-05 21:31:00 +0000

    Adding O(1) Space Solution for Middle of the Linked List

--- middle_of_the_linked_list.py
+++ middle_of_the_linked_list.py
@@ -12,12 +12,29 @@
 class Solution:
     def middleNode(self, head: ListNode) -> ListNode:
         result = []
-        i = 1
         while head:
             result.append(head)
-            i += 1
             head = head.next
         if len(result) < 3: 
             return result[-1]
         else:
             return result[(len(result)//2)]
+
+#O(1) Space:
+
+    def middleNode(self, head: ListNode) -> ListNode:
+        count = 0
+        dummy = head
+        while head:
+            count += 1
+            head = head.next
+        count = (count // 2) + 1 # or math.ceil()
+        if count == 0:
+            return dummy.next
+        i = 1
+        print(count)
+        while dummy:
+            if count == i:
+                return dummy
+            i += 1
+            dummy = dummy.next
