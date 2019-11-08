a640cb1290477601e4c11cce8366a8e3d3d74442
Benjamin Wiens <benjamin.wiens@gmail.com>
2019-11-08 20:06:59 +0000

    Better Version of Two Sum II Input is Sorted

--- two_sum_ii_input_is_sorted.py
+++ two_sum_ii_input_is_sorted.py
@@ -6,14 +6,15 @@ numbers = [2,7,11,15]
 target = 9
 class Solution:
     def twoSum(self, numbers, target):
-        right = len(numbers) - 1
-        left = 0
-        while left < right:
-            if numbers[left] + numbers[right] < target:
-                left += 1
-            elif numbers[left] + numbers [right] > target: 
-                right -= 1
+        l = 0
+        r = len(numbers)-1
+        while l < r:
+            s = numbers[l] + numbers[r]
+            if s == target:
+                return (l+1, r+1)
+            if s < target:
+                l += 1
             else:
-                return [left+1,right+1]
+                r -= 1
 print(Solution().twoSum(numbers,target))
 #O(n) time, #O(1) space
