90d33993412b13b9011c9dc7b9856ed91eb4022a
Benjamin Wiens <benjamin.wiens@gmail.com>
2019-11-19 18:49:29 +0000

    Better Version of Minimum Domino Rotations for Equal Row

--- minimum_domino_rotations_for_equal_row.py
+++ minimum_domino_rotations_for_equal_row.py
@@ -5,37 +5,35 @@
 A = [2,1,2,4,2,2]
 B = [5,2,6,2,3,2]
 
-import sys
 class Solution:
     def minDominoRotations(self, A, B):
-        count, result = 0, sys.maxsize
-        for i in range(1, 7):
-            count = 0
-            rotations = 0
-            for idx, num in enumerate(A):
-                # process row A
-                if num == i:
-                    count += 1
-                elif B[idx] == i:
-                        rotations += 1
-            if count == len(A):
-                return 0
-            if rotations + count == len(A):
-                result = min(result, rotations)
-            count = 0
-            rotations = 0
-            for idx, num in enumerate(B):
-                # process row A
-                if num == i:
-                    count += 1
-                elif A[idx] == i:
-                        rotations += 1
-            if count == len(A):
-                return 0
-            if rotations + count == len(A):
-                result = min(result, rotations)
-        if result == sys.maxsize:
-            return -1
-        return result
-
+        result = []
+        for i in range(2):
+            if i == 0:
+                k = A[0]
+            else:
+                k = B[0]
+            count_a = 0
+            rotate_a = 0
+            count_b = 0
+            rotate_b = 0
+            for j in range(len(A)):
+                # side A
+                if A[j] == k:
+                    count_a += 1
+                elif B[j] == k:
+                    rotate_a += 1
+                # side B
+                if B[j] == k:
+                    count_b += 1
+                elif A[j] == k:
+                    rotate_b += 1
+            # Both Sides might be equal with rotating. return the minimum
+            if count_a + rotate_a == len(A):    
+                result.append(min(count_a, rotate_a))
+            if count_b + rotate_b == len(A):    
+                result.append(min(count_b, rotate_b))
+            if result:
+                return min(result)
+        return -1
 print(Solution().minDominoRotations(A,B))
