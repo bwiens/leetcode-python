7c75fade6fd16d5725a2d4cbf310919da6b5c9b7
Benjamin Wiens <benjamin.wiens@gmail.com>
2019-11-24 19:02:24 +0000

    Better Version of License Key Formatting

--- license_key_formatting.py
+++ license_key_formatting.py
@@ -4,42 +4,19 @@
 #Leetcode Accepted
 input = "2-4A0r7-4k"
 K = 4
+
 class Solution:
-    def licenseKeyFormatting(self, S, K):
-        """
-        :type S: str
-        :type K: int
-        :rtype: str
-        """
-        list2, result = [], []
-        formatted = ""
-        S = S.upper()
-        list2 = list(S)
-        list2.reverse()
-        c = 0
-        Flag = False
-        for i in list2:
-            if c == K:
-                if i == "-":
-                    result.append(i)
-                    c = 0
-                else:
-                    result.append("-")
-                    result.append(i)
-                    c = 1
-                    Flag = True
-            if c < K and Flag == False:
-                if i != "-":
-                    result.append(i)
-                    c+=1
-            Flag = False
-        #special cases
-        if len(result) > 0:
-            if result[-1] == "-":
-                result.pop()
-        result.reverse()
-        #format back to string
-        formatted = ''.join(result)
-        return formatted
+    def licenseKeyFormatting(self, S: str, K: int) -> str:
+        S2 = S.replace('-', '')
+        result = ''
+        for index, char in enumerate(S2[::-1], 1):
+            result += char.upper()
+            if index % K == 0 and index != len(S2):
+                result += '-'
+        result = result[::-1]
+        if not result: return ""
+        if result[-1] == '-':
+            return result[:len(result)-1]
+        return result
 
 print(Solution().licenseKeyFormatting(input, K))
