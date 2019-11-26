e6fe47c7033df2e37d6ae792359ee5caa3e9f550
Benjamin Wiens <benjamin.wiens@gmail.com>
2019-11-26 16:33:34 +0000

    Input Typo

--- sentence_similarity_ii.py
+++ sentence_similarity_ii.py
@@ -7,7 +7,7 @@ words2 = ["one","really","good","dinner"]
 pairs = [["great","good"],["extraordinary","good"],["well","good"],["wonderful","good"],["excellent","good"],["fine","good"],["nice","good"],["any","one"],["some","one"],["unique","one"],["the","one"],["an","one"],["single","one"],["a","one"],["truck","car"],["wagon","car"],["automobile","car"],["auto","car"],["vehicle","car"],["entertain","have"],["drink","have"],["eat","have"],["take","have"],["fruits","meal"],["brunch","meal"],["breakfast","meal"],["food","meal"],["dinner","meal"],["super","meal"],["lunch","meal"],["possess","own"],["keep","own"],["have","own"],["extremely","very"],["actually","very"],["really","very"],["super","very"]]
 
 class Solution:
-    def areSentencesSimilarTwo(self, words1, words2):
+    def areSentencesSimilarTwo(self, words1, words2, pairs):
         if len(words1) != len(words2):
             return False
         hmap = {}
