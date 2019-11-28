9d5a82393a10cbb33d24f304cd879acd2356c5d5
Benjamin Wiens <benjamin.wiens@gmail.com>
2019-11-28 17:53:36 +0000

    Adding Better Version of Number of Closed Islands

--- number_of_closed_islands.py
+++ number_of_closed_islands.py
@@ -5,14 +5,6 @@
 grid =[ [1,1,1,1,1,1,1,0],[1,0,0,0,0,1,1,0],[1,0,1,0,1,1,1,0],[1,0,0,0,0,1,0,1],[1,1,1,1,1,1,1,0]]
 class Solution:
     def closedIsland(self, grid):
-        def close(rid, cid, grid):
-            if rid < 0 or cid < 0 or rid > len(grid)-1 or cid > len(grid[0])-1 or grid[rid][cid] != 0:
-                return
-            grid[rid][cid] = 1
-            close(rid+1, cid, grid)
-            close(rid, cid+1, grid)
-            close(rid-1, cid, grid)
-            close(rid, cid-1, grid)
         def countI(rid, cid, grid):
             if rid < 0 or cid < 0 or rid > len(grid)-1 or cid > len(grid[0])-1 or grid[rid][cid] != 0:
                 return
@@ -26,7 +18,7 @@ class Solution:
             for cid, column in enumerate(row):
                 if cid == 0 or rid == 0 or rid == len(grid)-1 or cid == len(grid[0])-1:
                     if column == 0:
-                        close(rid,cid,grid)
+                        countI(rid,cid,grid)
         # cound number of closed islands
         count = 0
         for rid, row in enumerate(grid):
