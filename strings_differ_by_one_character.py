275e54dbd5243dce8850e2a69348429837dc6328
Benjamin Wiens <benjamin.wiens@gmail.com>
2020-08-22 18:35:53 +0000

    Better Version of Strings Differ by One Character

--- strings_differ_by_one_character.py
+++ strings_differ_by_one_character.py
@@ -4,20 +4,14 @@ dict = ["abcd","acbd", "aacd"]
 
 class Solution:
     def differByOne(self, dict):
-        for index, key in enumerate(dict):
-            for index2, key2 in enumerate(dict):
-                if index != index2:
-                    if len(key) == len(key2):
-                        foundOne = False
-                        for index, char in enumerate(key):
-                            if char != key2[index]:
-                                if not foundOne:
-                                    foundOne = True
-                                else:
-                                    break
-                            if index == len(key) -1 and foundOne:
-                                return True
+        combos = set()
+        for key in dict:
+            for i in range(len(key)):
+                combo = key[:i] + '*' + key[i+1:]
+                if combo in combos:
+                    return True
+                combos.add(combo)
         return False
-                        
+
 print(Solution().differByOne(dict))                            
                             
