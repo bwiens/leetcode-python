bdf929903f86b729d58c0c4d62389fe05cc225bb
Benjamin Wiens <benjamin.wiens@gmail.com>
2020-09-05 14:39:22 +0000

    Better Version of K Closest Points to Origin

--- k_closest_points_to_origin.py
+++ k_closest_points_to_origin.py
@@ -2,23 +2,21 @@
 # Benjamin Wiens
 # K Closest Points to Origin (https://leetcode.com/problems/k-closest-points-to-origin/submissions/)
 # Leetcode Accepted
-import math
+import heapq
 
 coordinates = [[3,3],[5,-1],[-2,4]]
 K = 2
 class Solution:
     def kClosest(self, points, K):
-        distance = []
-        for p in points:
-            #this is based on pythagoreon theorem
-            x = math.sqrt(p[0]**2 + p[1]**2)
-            #create tuple of distance and coordinate
-            distance.append((x, p))
-        #sort by distance
-        distance.sort(key=lambda x: x[0])
-        #print second part of tuple
-        return [x[1] for x in distance[:K]]
-    #O(nlogn) time
-    #O(n) space
-
+        heap = []
+        for point in points:
+            dist = point[0] * point[0] + point[1] * point[1] 
+            # adding negative so the largest number becomes smallest
+            heapq.heappush(heap,(-dist, point[0], point[1]))
+            if len(heap) > K:
+                heapq.heappop(heap)
+        result = []
+        for x, y, z in heap:
+            result.append((y, z))
+        return result
 print(Solution().kClosest(coordinates,K))
