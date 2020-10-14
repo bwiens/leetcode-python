9880b918a62f5a63c8710ef4049d29d9f2f43729
Benjamin Wiens <benjamin.wiens@gmail.com>
2020-10-14 01:07:49 +0000

    Better Version of Valid Anagram

--- valid_anagram.py
+++ valid_anagram.py
@@ -5,36 +5,13 @@
 s = "anagram"
 t = "nagaram"
 class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-        #create dict with occurences:
-        if s and not t:
-            return False
-        wdict = {}
-        value = 0
-        for i in t:
-            if i not in wdict:
-                wdict[i] = 1
-            else:
-                value = wdict.get(i)
-                wdict[i] = value + 1
-                
-        for i in s:
-            if i not in wdict:
-                return False
-            else:
-                value = wdict.get(i)
-                if value < 1:
-                    return False
-                if value == 1:
-                    del wdict[i]
-                else:
-                    wdict[i] = value -1
-                    
-        #if we have letters left in the dict, we do
-        #not have a valid anagram
-        if len(wdict) > 0:
-            return False
-        else:
-            return True
+    def isAnagram(self, s: str, t: str):
+        count1 = [0] * 26
+        count2 = [0] * 26
+        for c in s:
+            count1[ord(c) - ord('a')] += 1
+        for c in t:
+            count2[ord(c) - ord('a')] += 1
+        return count1 == count2
 print(Solution().isAnagram(s, t))
 #O(n) Runtime (linear), fastest solituin, with constant space O(1) 
