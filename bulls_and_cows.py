160bdc2d55064c964e40d924d4ec4c135de9b2a2
Benjamin Wiens <benjamin.wiens@gmail.com>
2020-10-29 13:31:28 +0000

    Adding better Version of Bulls and Cows

--- bulls_and_cows.py
+++ bulls_and_cows.py
@@ -3,32 +3,25 @@
 #https://leetcode.com/problems/bulls-and-cows/
 secret = "1123"
 guess =  "0111"
-
 class Solution:
-    def getHint(self, secret, guess):
-        """
-        :param secret: str
-        :param guess: str
-        :return: str
-        """
-        bulls = 0
-        cows = 0
-        #create hash tables with incremented idex
-        guess_map = dict(enumerate(guess))
-        secret_map = dict(enumerate(secret))
-        #find the ones (bulls) in position
-        for key, value in secret_map.items():
-            if guess_map.get(key)  == value:
-                #overwrite value
-                secret_map[key] = 'X'
-                guess_map[key] = 'A'
+    def getHint(self, secret: str, guess: str) -> str:
+        bulls, cows = 0, 0
+        dict = {}
+        for index, char in enumerate(secret):
+            if guess[index] == char:
                 bulls += 1
-        for key, value in secret_map.items():
-            for key2, value2 in guess_map.items():
-                if value == value2:
-                    #overwrite value
-                    guess_map[key2] = 'Z'
+            else:
+                if char in dict:
+                    dict[char] += 1
+                else:
+                    dict[char] = 1
+        for index, char in enumerate(secret):
+            #non bull:
+            if guess[index] != char and guess[index] in dict:
                     cows += 1
-                    break
-        return str(bulls) + 'A' + str(cows) + 'B'
+                    dict[guess[index]] -= 1
+                    if dict[guess[index]] == 0:
+                        del dict[guess[index]]
+        return str(bulls)+'A'+str(cows)+'B'
+
 print(Solution().getHint(secret, guess))
